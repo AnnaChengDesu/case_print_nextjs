aa0c67678ebf187c03af06c5a24501e5352c2661
Anna Cheng <zan272905918@163.com>
2024-09-01 18:23:38 +0800

    Landing page done

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
+
+export default function MaxWithWrapper({
+  children,
+  className,
+}: {
+  children: ReactNode
+  className?: string
+}) {
+  return (
+    <div className={cn('h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+      {children}
+    </div>
+  )
+}
